84e92950673734bdd607788f24e5265bf030326a
lelinrashed <lelin.rashed784@gmail.com>
2019-05-09 19:41:14 +0600

    Push after setup the basic blog

---
+++ blog/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+from blog.models import BlogPost
+
+def blog_post_list_view(request):
+    # List out objects
+    # ? could be search
+    qs = BlogPost.objects.all()
+    template_name = 'blog/list.html'
+    context = {'object_list': qs}
+    return render(request, template_name, context)
+
+
+def blog_post_create_view(request):
+    # create objects
+    # ? use a form
+
+    template_name = 'blog/create.html'
+    context = {'form': None}
+    return render(request, template_name, context)
+
+
+def blog_post_detail_view(request, slug):
+    # 1 object -> detail view
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog/detail.html'
+    context = {'object': obj}
+    return render(request, template_name, context)
+
+
+def blog_post_update_view(request, slug):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog/update.html'
+    context = {'object': obj, 'form': None}
+    return render(request, template_name, context)
+
+
+def blog_post_delete_view(request, slug):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog/delete.html'
+    context = {'object': obj}
+    return render(request, template_name, context)
+
